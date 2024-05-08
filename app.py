6b25a28604b0fd4dc4481f4abfe065b95630bbe9
karinmashraki <carin1233@walla.com>
2024-05-08 17:06:54 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from sklearn.impute import SimpleImputer
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.combine import SMOTEENN
-
+from sklearn.pipeline import Pipeline
 
 from pycaret.classification import *
 
@@ -25,24 +25,31 @@ st.markdown(
 
 st.markdown("<h3 style='text-align:center; color:black;'>Classification</h3>", unsafe_allow_html=True)
 
-
-
-
-
-
-
 df = None
 if 'df' not in st.session_state:
     st.session_state.df = df
 else:
     df = st.session_state.df
 
-if 'clicked' not in st.session_state:
-    st.session_state.clicked = False
 
+# if 'clicked' not in st.session_state:
+#     st.session_state.clicked = False
+#
+#
+# def click_button():
+#     st.session_state.clicked = True
 
-def click_button():
-    st.session_state.clicked = True
+def stateful_button(*args, key=None, **kwargs):
+    if key is None:
+        raise ValueError("Must pass key")
+
+    if key not in st.session_state:
+        st.session_state[key] = False
+
+    if st.button(*args, **kwargs):
+        st.session_state[key] = not st.session_state[key]
+
+    return st.session_state[key]
 
 
 def random_imputation(new_data2, missing_columns):
@@ -62,7 +69,6 @@ if file is not None:
     st.write("Dataset shape:", df.shape)
     chosen_target = st.selectbox("Choose your target variable", df.columns.tolist())
     st.write("chosen target is ", chosen_target)
-
 
 with st.expander("Exploratory Data Analysis"):
     if df is not None:
@@ -150,30 +156,30 @@ with st.expander("Data Cleaning"):
             columns = st.multiselect("Select columns to drop", df.columns)
         if Handling_missing_data:
             Handling_missing_data_operation = st.selectbox('Please pick the operation you want',
-                                                           ['', 'Drop the missing data', 'Fill the missing data'])
-            if 'Drop the missing data' in Handling_missing_data_operation:
+                                                           ['', 'Drop Missing Values', 'Missing Values Imputation'])
+            if 'Drop Missing Values' in Handling_missing_data_operation:
                 drop_na0 = st.checkbox("Drop all rows with Nan values")
                 drop_na1 = st.checkbox("Drop all columns with Nan values")
-            if 'Fill the missing data' in Handling_missing_data_operation:
+            if 'Missing Values Imputation' in Handling_missing_data_operation:
                 cat_col = [col for col in df.columns if df[col].dtype == 'object']
                 num_col = [col for col in df.columns if df[col].dtype != 'object']
                 columns_with_empty_values = df.columns[df.isnull().any()].tolist()
                 numeric_columns_with_empty_values = df[num_col].columns[df[num_col].isnull().any()].tolist()
                 categorial_columns_with_empty_values = df[cat_col].columns[df[cat_col].isnull().any()].tolist()
                 Imputation_options = st.multiselect("Select the imputations you want", options=(
-                    "Mean", "Median", "Most frequent", "Random Imputation"))
+                    "Mean Imputation", "Median Imputation", "Most frequent Imputation", "Random Imputation"))
 
-                if "Mean" in Imputation_options:
+                if "Mean Imputation" in Imputation_options:
                     columns_for_Mean_imputation = st.multiselect(
                         "Please select the columns that you want to perform Mean imputation on",
                         numeric_columns_with_empty_values)
 
-                if "Median" in Imputation_options:
+                if "Median Imputation" in Imputation_options:
                     columns_for_Median_imputation = st.multiselect(
                         "Please select the columns that you want to perform Median imputation on",
                         numeric_columns_with_empty_values)
 
-                if "Most frequent" in Imputation_options:
+                if "Most frequent Imputation" in Imputation_options:
                     columns_for_Most_frequent_imputation = st.multiselect(
                         "Please select the columns that you want to perform Most frequent imputation on",
                         columns_with_empty_values)
@@ -184,30 +190,31 @@ with st.expander("Data Cleaning"):
                         columns_with_empty_values)
 
         # st.button('Apply data cleaning', on_click=click_button)
-        if st.button('Apply data cleaning'):
+        # if st.button('Apply data cleaning'):
         # if st.session_state.clicked:
+        if stateful_button('Apply data cleaning', key="cleaning_button"):
             if drop_duplicates:
                 df = df.drop_duplicates()
             if drop_colmuns:
                 df = df.drop(columns, axis=1)
             if Handling_missing_data:
-                if 'Drop the missing data' in Handling_missing_data_operation:
+                if 'Drop Missing Values' in Handling_missing_data_operation:
                     if drop_na0:
                         df = df.dropna(axis=0)
                     if drop_na1:
                         df = df.dropna(axis=1)
-                if 'Fill the missing data' in Handling_missing_data_operation:
-                    if "Mean" in Imputation_options:
+                if 'Missing Values Imputation' in Handling_missing_data_operation:
+                    if "Mean Imputation" in Imputation_options:
                         imputer = SimpleImputer(strategy='mean')
                         df[columns_for_Mean_imputation] = np.round(
                             imputer.fit_transform(df[columns_for_Mean_imputation]), 1)
 
-                    if "Median" in Imputation_options:
+                    if "Median Imputation" in Imputation_options:
                         imputer = SimpleImputer(strategy='median')
                         df[columns_for_Median_imputation] = np.round(
                             imputer.fit_transform(df[columns_for_Median_imputation]), 1)
 
-                    if "Most frequent" in Imputation_options:
+                    if "Most frequent Imputation" in Imputation_options:
                         imputer = SimpleImputer(strategy='most_frequent')
                         df[columns_for_Most_frequent_imputation] = imputer.fit_transform(
                             df[columns_for_Most_frequent_imputation])
@@ -219,8 +226,6 @@ with st.expander("Data Cleaning"):
             st.write(df)
             st.write("Dataset shape:", df.shape)
             st.write("Total number of Nan values remaining:", df.isna().sum().sum())
-
-       
 
 with st.expander("Data Preprocessing"):
     if df is not None:
@@ -251,27 +256,24 @@ with st.expander("Data Preprocessing"):
         if balance:
             sample_tech = st.selectbox("Select sampling technique", ["Over Sampling", "Under Sampling", "Combined"])
         # st.button('Apply data preprocessing', on_click=click_button)
-        if st.button('Apply data preprocessing'):
+        # if st.button('Apply data preprocessing'):
         # if st.session_state.clicked:
+        if stateful_button('Apply data preprocessing', key="preprocessing_button"):
             if Scale:
                 scaler = MinMaxScaler()
                 df[columns_for_scaling] = scaler.fit_transform(df[columns_for_scaling])
-
 
             if Encode:
                 if "Label Encoder" in encoding_options:
                     encoder = LabelEncoder()
                     df[columns_for_label] = df[columns_for_label].apply(encoder.fit_transform)
 
-
                 if "One Hot Encoder" in encoding_options:
                     df = pd.get_dummies(df, columns=columns_for_onehot, prefix=columns_for_onehot, drop_first=True)
-
 
                 if "Ordinal Encoder" in encoding_options:
                     encoder = OrdinalEncoder()
                     df[columns_for_Ordinal] = encoder.fit_transform(df[columns_for_Ordinal])
-
 
             if balance:
                 X = df.drop(chosen_target, axis=1)
@@ -297,18 +299,119 @@ with st.expander("Model Training"):
         st.write("Dataset to be used for training")
         st.write(df)
         st.write("Dataset shape:", df.shape)
-        N=5
- 
-        if st.button('Run Modelling'):
+        # setup(df, target=chosen_target, verbose=False)
+        # setup_df = pull()
+        # st.info("This is the ML experiment settings")
+        # st.dataframe(setup_df)
+        N = 5
+        # st.button('Run Modelling', on_click=click_button)
+        # if st.session_state.clicked:
+        if stateful_button('Run Modelling', key="modelling_button"):
+            # if st.button('Run Modelling'):
             setup(df, target=chosen_target, verbose=False)
             setup_df = pull()
             st.info("This is the ML experiment settings")
             st.dataframe(setup_df)
+
+            # pipeline = get_config('pipeline')
+            # pipeline
+            # st.success(pipeline)
+            # st.info(pipeline)
+            #
+            # Transformed_dataset = get_config('dataset_transformed')
+            # st.subheader('Transformed dataset:')
+            # st.dataframe(Transformed_dataset)
+            # st.write('Transformed dataset shape: ', Transformed_dataset.shape)
+            #
+            # Transformed_train = get_config('train_transformed')
+            # st.subheader('Transformed train:')
+            # st.dataframe(Transformed_train)
+            # st.write('Transformed train shape: ', Transformed_train.shape)
+            #
+            # Transformed_test = get_config('test_transformed')
+            # st.subheader('Transformed test:')
+            # st.dataframe(Transformed_test)
+            # st.write('Transformed test shape: ', Transformed_test.shape)
+
             best_N = compare_models(n_select=N)
             compare_best_N = pull()
             best_model = best_N[0]
+
+            # st.subheader("Before Tuning")
             st.info("The performance of all the estimators available in the model library")
             st.dataframe(compare_best_N)
+            # fig = px.bar(
+            #     compare_best_N.set_index("Model"),
+            #     orientation='h',
+            #     width=980
+            # )
+            # fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), yaxis={'categoryorder': 'total ascending'})
+            # st.write(fig)
+
+            # best_model = compare_models()
+            # compare_df = pull()
+            # st.info("This is your ML model")
+            # st.dataframe(compare_df)
+
+            # fig = px.bar(
+            #     compare_df.set_index("Model"),
+            #     orientation='h',
+            #     width=980
+            # )
+            # fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), yaxis={'categoryorder': 'total ascending'})
+            # st.write(fig)
+
+            # st.info("The 5 best estimators are:")
+            # for model in best_N:
+            #     model
+            # st.info("The winner algorithm is:")
+            # best_model
+            # model_name = type(best_model).__name__
+
+            # # Get all plots
+            # all_plots = list(get_config('_available_plots').keys())
+            # all_plots
+            # # plotting = st.multiselect("Please pick the plots you want", all_plots)
+            # for plot in all_plots:
+            #     try:
+            #         # Save each plot of each model to desired directory
+            #         plot_model(best_model, plot=plot, display_format='streamlit')
+            #     except Exception as e:
+            #         st.info(f'{model_name} has error in {plot} plot')
+
+            # available_plots = get_config('_available_plots')
+            # available_plots
+            # st.write(type(available_plots))
+
+            # plot_model(best_model, plot='confusion_matrix', plot_kwargs={'percent': True},
+            #            display_format='streamlit')
+            #
+            # plot_model(best_model, plot='parameter', display_format='streamlit')
+            # pl = ['parameter', 'confusion_matrix', 'auc', 'threshold', 'pr', 'class_report', 'learning', 'manifold']
+            # for plot in pl:
+            #     try:
+            #         plot_model(best_model, plot=plot, display_format='streamlit')
+            #     except:
+            #         pass
+
+            # st.header("Predictions")
+            # predictions = predict_model(best_model, raw_score=True)
+            # st.write('#### Predictions on holdout set')
+            # st.dataframe(predictions)
+            #
+            # new_data = df.copy()
+            # new_data.drop(chosen_target, axis=1, inplace=True)
+            #
+            # predictions = predict_model(best_model, data=new_data, raw_score=True)
+            # st.write('#### Predictions on new data')
+            # st.dataframe(predictions)
+
+            # tuned_best_model = tune_model(best_model)
+            # st.info("Tuned model:")
+            # tuned_best_model
+            # if st.button('Tune 5 best models'):
+            # Then tune these N best models
+            # st.subheader("After Tuning the hyperparameters of the 5 best estimators")
             tuned_models = [tune_model(model) for model in best_N]
             best_model = compare_models(include=tuned_models)
             compare_df = pull()
@@ -323,8 +426,9 @@ with st.expander("Model Training"):
             st.write(fig)
             st.info("The winner algorithm after Hyperparameter tuning is:")
             best_model
+            # st.info(best_model)
             plot_model(best_model, plot='confusion_matrix', plot_kwargs={'percent': True},
-                        display_format='streamlit')
+                       display_format='streamlit')
 
             plot_model(best_model, plot='parameter', display_format='streamlit')
             pl = ['confusion_matrix', 'auc', 'threshold', 'pr', 'class_report', 'learning', 'manifold']
@@ -334,9 +438,7 @@ with st.expander("Model Training"):
                 except:
                     pass
 
-
             save_model(best_model, 'best_model')
 
             with open('best_model.pkl', 'rb') as model_file:
                 st.download_button('Download the model', model_file, 'best_model.pkl')
-
